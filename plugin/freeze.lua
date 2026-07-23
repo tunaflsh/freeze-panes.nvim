1c158b5852a47bee871ec150e5820bc748c80a94
tunaflsh <tunaflish.ai@gmail.com>
2026-07-23 18:37:04 +0200

    feat: per window freeze

--- plugin/freeze.lua
+++ plugin/freeze.lua
@@ -1,18 +1,24 @@
 local config = require('freeze')
-local freeze
+local freeze = {}
+
+local function close(win)
+  vim.api.nvim_win_close(freeze[win], true)
+  vim.api.nvim_del_augroup_by_name('freeze.' .. win)
+end
 
 vim.api.nvim_create_user_command('Freeze', function(o)
-  if freeze and vim.api.nvim_win_is_valid(freeze) then
-    vim.api.nvim_win_close(freeze, true)
-    vim.api.nvim_del_augroup_by_name('freeze')
-    return
-  end
-
   local buf = vim.fn.bufnr()
   local win = vim.fn.win_getid()
   local height = o.line2 - o.line1 + 1
 
-  freeze = vim.api.nvim_open_win(0, false, {
+  if freeze[win] and vim.api.nvim_win_is_valid(freeze[win]) then
+    close(win)
+    if o.range == 0 then
+      return
+    end
+  end
+
+  freeze[win] = vim.api.nvim_open_win(0, false, {
     relative = 'win',
     win = win,
     row = 0,
@@ -25,26 +31,31 @@ vim.api.nvim_create_user_command('Freeze', function(o)
     zindex = 99,
   })
 
-  vim.api.nvim_create_augroup('freeze', {})
+  local group = vim.api.nvim_create_augroup('freeze.' .. win, {})
   vim.api.nvim_create_autocmd('CursorMoved', {
-    group = 'freeze',
+    group = group,
     buf = buf,
     callback = function()
-      vim.api.nvim_win_set_config(freeze, {
+      vim.api.nvim_win_set_config(freeze[win], {
         hide = win == vim.fn.win_getid() and vim.fn.winline() <= height + math.min(1, #config.sep),
       })
     end,
   })
   vim.api.nvim_create_autocmd('WinResized', {
-    group = 'freeze',
+    group = group,
     callback = function()
-      vim.api.nvim_win_set_config(freeze, { width = vim.api.nvim_win_get_width(win) })
+      vim.api.nvim_win_set_config(freeze[win], { width = vim.api.nvim_win_get_width(win) })
     end,
   })
   vim.api.nvim_create_autocmd('WinScrolled', {
-    group = 'freeze',
+    group = group,
     callback = function()
     end,
+  })
+  vim.api.nvim_create_autocmd('WinClosed', {
+    group = group,
+    pattern = tostring(win),
+    callback = function() close(win) end,
   })
 end, {
   range = true,

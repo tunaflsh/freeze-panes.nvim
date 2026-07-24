4809e5b74dc0746df2addac874234596c2ec6cbb
tunaflsh <tunaflish.ai@gmail.com>
2026-07-24 03:19:37 +0200

    feat: sync folds

--- plugin/freeze.lua
+++ plugin/freeze.lua
@@ -58,8 +58,9 @@ for _, dir in ipairs({ '', 'V' }) do
         zindex = dir == 'V' and 49 or 50,
       })
       vim.wo[freeze[dir][win]].signcolumn = 'no'
+      vim.wo[freeze[dir][win]].foldmethod = 'manual'
       if dir == '' then
-        vim.wo[freeze[dir][win]].foldlevel = 99
+        vim.wo[freeze[dir][win]].foldenable = false
       end
       vim.api.nvim_win_call(freeze[dir][win], function()
         vim.api.nvim_win_set_cursor(freeze[dir][win], { row1, col1 })
@@ -67,21 +68,12 @@ for _, dir in ipairs({ '', 'V' }) do
       end)
 
       local group = vim.api.nvim_create_augroup('freeze.' .. freeze[dir][win], {})
-      vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
+      vim.api.nvim_create_autocmd('WinClosed', {
         group = group,
+        pattern = { tostring(win), tostring(freeze[dir][win]) },
         callback = function()
-          local hide
-          if win ~= vim.fn.win_getid() then
-            return
-          elseif buf ~= vim.api.nvim_win_get_buf(win) then
-            hide = true
-          elseif dir == 'V' then
-            local w = vim.fn.getwininfo(win)[1]
-            hide = vim.fn.wincol() <= width + math.min(1, #config.sep[dir]) + w.textoff
-          else
-            hide = vim.fn.winline() <= height + math.min(1, #config.sep[dir])
-          end
-          vim.api.nvim_win_set_config(freeze[dir][win], { hide = hide })
+          vim.api.nvim_win_close(freeze[dir][win], false)
+          vim.api.nvim_del_augroup_by_id(group)
         end,
       })
       vim.api.nvim_create_autocmd('WinResized', {
@@ -108,44 +100,97 @@ for _, dir in ipairs({ '', 'V' }) do
             local w = vim.fn.getwininfo(win)[1]
             vim.api.nvim_win_call(freeze[dir][win], function()
               vim.api.nvim_win_set_cursor(freeze[dir][win], { w.topline, col1 })
-              vim.fn.winrestview({ topline = w.topline })
+              vim.fn.winrestview({ topline = w.topline, leftcol = col1 })
             end)
           elseif dir == '' and event.leftcol ~= 0 then
             local w = vim.fn.getwininfo(win)[1]
             vim.api.nvim_win_call(freeze[dir][win], function()
-              vim.fn.winrestview({ leftcol = w.leftcol })
+              vim.fn.winrestview({ topline = row1, leftcol = w.leftcol })
             end)
           end
         end,
       })
 
-      local textoff_updater = vim.uv.new_timer()
-      if textoff_updater then
-        local textoff = wininfo.textoff
-        textoff_updater:start(150, 150, vim.schedule_wrap(function()
-          local w = vim.fn.getwininfo(win)[1]
-          if textoff ~= w.textoff then
-            textoff = w.textoff
-            vim.api.nvim_win_set_config(freeze[dir][win], {
-              win = win,
-              relative = 'win',
-              row = 0,
-              col = w.textoff,
-              width = dir == '' and w.width - w.textoff or nil,
-            })
-          end
-        end))
-      end
-
-      vim.api.nvim_create_autocmd('WinClosed', {
+      vim.api.nvim_create_autocmd('CursorMoved', {
         group = group,
-        pattern = { tostring(win), tostring(freeze[dir][win]) },
         callback = function()
-          vim.api.nvim_win_close(freeze[dir][win], false)
-          vim.api.nvim_del_augroup_by_id(group)
-          if textoff_updater then
-            textoff_updater:stop()
-            textoff_updater:close()
+          if win ~= vim.fn.win_getid() then
+            return
+          end
+
+          local hide
+          local w = vim.fn.getwininfo(win)[1]
+          if buf ~= vim.api.nvim_win_get_buf(win) then
+            hide = true
+          elseif dir == 'V' then
+            hide = vim.fn.wincol() <= width + math.min(1, #config.sep[dir]) + w.textoff
+          else
+            hide = vim.fn.winline() <= height + math.min(1, #config.sep[dir])
+          end
+
+          vim.api.nvim_win_set_config(freeze[dir][win], {
+            win = win,
+            relative = 'win',
+            row = 0,
+            col = w.textoff,
+            width = dir == '' and w.width - w.textoff or nil,
+            hide = hide
+          })
+
+          if not hide and dir == 'V' then
+            local lnum = vim.api.nvim_buf_line_count(buf)
+            local folds = {}
+            vim.api.nvim_win_call(win, function()
+              local line = 1
+              while line <= lnum do
+                local open = vim.fn.foldclosed(line)
+                if open ~= -1 then
+                  local close = vim.fn.foldclosedend(line)
+                  table.insert(folds, { open, close })
+                  line = close
+                end
+                line = line + 1
+              end
+            end)
+
+            vim.api.nvim_win_call(freeze[dir][win], function()
+              local skip = true
+              local line = 1
+              for _, fold in ipairs(folds) do
+                if fold[1] ~= vim.fn.foldclosed(fold[1]) then
+                  skip = false break
+                elseif fold[2] ~= vim.fn.foldclosed(fold[1]) then
+                  skip = false break
+                elseif line < fold[1]
+                  and vim.iter(vim.fn.range(line, fold[1] - 1)):any(function(l)
+                    return vim.fn.foldclosed(l) ~= -1
+                  end) then
+                  skip = false break
+                end
+                line = fold[2] + 1
+              end
+              if not skip then
+              elseif line >= lnum then
+                return
+              elseif
+                vim.iter(vim.fn.range(line, lnum)):all(function(l)
+                  return vim.fn.foldclosed(l) == -1
+                end) then
+                return
+              end
+
+              vim.cmd('normal! zE')
+              line = 1
+              for _, fold in ipairs(folds) do
+                pcall(vim.cmd, ('%d,%dfoldopen!'):format(line, fold[2]))
+                vim.cmd(('%d,%dfold'):format(fold[1], fold[2]))
+                line = fold[2] + 1
+              end
+              pcall(vim.cmd, ('%d,$foldopen!'):format(line))
+
+              vim.api.nvim_win_set_cursor(freeze[dir][win], { w.topline, col1 })
+              vim.fn.winrestview({ topline = w.topline, leftcol = col1 })
+            end)
           end
         end,
       })
